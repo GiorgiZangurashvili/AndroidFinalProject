4f025187d2c4eb4806a65cf812e639639d694b9b
gzang20 <91743469+zangura77@users.noreply.github.com>
2023-07-30 18:02:04 +0400

    UserAdapter updated

--- app/src/main/java/gobejishvili/zangurashvili/finalproject/adapter/UserAdapter.kt
+++ app/src/main/java/gobejishvili/zangurashvili/finalproject/adapter/UserAdapter.kt
@@ -1,13 +1,17 @@
 package gobejishvili.zangurashvili.finalproject.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import gobejishvili.zangurashvili.finalproject.activity.ChatActivity
 import gobejishvili.zangurashvili.finalproject.databinding.UserItemBinding
 import gobejishvili.zangurashvili.finalproject.entity.User
 
-class UserAdapter(val users: ArrayList<User>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(val users: ArrayList<User>, val mAuth: FirebaseAuth): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     class UserViewHolder(binding: UserItemBinding): RecyclerView.ViewHolder(binding.root){
         val username = binding.username
@@ -19,7 +23,7 @@ class UserAdapter(val users: ArrayList<User>): RecyclerView.Adapter<UserAdapter.
         return UserViewHolder(
             UserItemBinding.inflate(
                 LayoutInflater.from(parent.context),
-            parent, false))
+                parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -33,6 +37,14 @@ class UserAdapter(val users: ArrayList<User>): RecyclerView.Adapter<UserAdapter.
         Glide
             .with(holder.itemView.context)
             .load(users[position].profilePictureUrl)
+            .apply(RequestOptions.circleCropTransform())
             .into(holder.profilePicture)
+
+        holder.itemView.setOnClickListener {
+            val intent: Intent = Intent(it.context, ChatActivity::class.java)
+            intent.putExtra("getterId", mAuth.currentUser?.uid)
+            intent.putExtra("senderId", currentUser.userId)
+            it.context.startActivity(intent)
+        }
     }
 }
